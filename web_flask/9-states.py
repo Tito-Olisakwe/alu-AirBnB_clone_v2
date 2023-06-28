2d36dfed85ca38aecad0c1ad5de93e83c6e96357
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2023-06-28 22:59:15 +0000

    task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-"""flask routes"""
+"""
+    python script that starts a Flask web application
+"""
+
 from models import storage
 from models.state import State
 from flask import Flask, render_template
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/states')
 @app.route('/states/<id>')
 def states_list(id=None):
+    """
+        Return: HTML page with list of states
+    """
+    path = '9-states.html'
     states = storage.all(State)
-    if id:
-        key = 'State.' + id
-        cities = []
-        if key in states:
-            state = states[key]
-            if storage_type == 'db':
-                cities = state.cities
-            else:
-                cities = state.cities()
-        return render_template('9-states.html', states=states, id=id, cities=cities)
-    else:
-        return render_template('9-states.html', states=states, id=id, cities=None)
+    return render_template(path, states=states, id=id)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def app_teardown(arg=None):
+    """
+        Clean-up session
+    """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
